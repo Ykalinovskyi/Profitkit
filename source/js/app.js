16190d7404fb6b6e3773d7ac425bf6b146b74ebb
Evgenyi Kalinovskyi <ykalinovskyi@gmail.com>
2023-02-11 11:51:37 +0300

    finish adaptive slider

---
+++ source/js/app.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+
+      navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+      },
+});
+
+
